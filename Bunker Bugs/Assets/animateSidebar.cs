2853c009685077854c5f34fc13a1b7c088f9585f
owenryanul <12128015@studentmail.ul.ie>
2017-01-26 17:15:26 +0000

    Dev Build 6
    +Added Sidebar(that will eventually hold player units for purchase)
    +Added Slide in and slide out animations for sidebar
    
    -ISSUE: Sidebar starts slide out and slide in on startup, should find
    way to start the sidebar slide in.
    -OLD Bug: -Enemies Occasionaly jump higher than normal after jumping at
    a 2 high
    wall multple times, allowing them to jump ontop of 2 high walls.
    (Untested with higher walls)

---
+++ Bunker Bugs/Assets/animateSidebar.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animateSidebar : MonoBehaviour {
+
+    private Animator animater;
+    private bool isOut;
+
+    // Use this for initialization
+    void Start() {
+        animater = this.GetComponent<Animator>();
+        animater.Play("Sidebar Out");
+        isOut = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public void toggleSidebar()
+    {
+        if (isOut)
+        {
+            slideInSidebar();
+            isOut = false;
+        }
+        else
+        {
+            slideOutSidebar();
+            isOut = true;
+        }
+    }
+
+    public void slideOutSidebar()
+    {
+        animater.Play("Sidebar Out");
+    }
+
+    public void slideInSidebar()
+    {
+        animater.Play("Sidebar In");
+    }
+}
