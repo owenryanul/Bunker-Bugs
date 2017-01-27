1713b556664073b7a8e1cd52a608eef0f6849ab9
owenryanul <12128015@studentmail.ul.ie>
2017-01-27 17:11:21 +0000

    Dev Build 7
    +Added Tile Placement Mechanics
    +Added Ghosts for Grunt and Steel Wall(used for placement)
    +Added obstruction detection to Tile Placement Mechanics
    +Added sidebar obstructing Tile Placement
    
    -New BUG: Enemies can jump up through the underside of tiles
    -New BUG: Grunts can be placed without a solid tile below them, should
    add ground detection to placement mechanics
    -ISSUE: Sidebar starts slide out and slide in on startup, should find
    way to start the sidebar slide in.
    -OLD Bug (UPDATE): -Enemies Occasionaly jump higher than normal after
    jumping at a 2 high wall multple times, allowing them to jump ontop of 2
    high walls. Update: cannot jump higher walls, bug caused by some jumps
    being very slighter higher than others.

---
+++ Bunker Bugs/Assets/GhostLogic.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostLogic : MonoBehaviour {
+
+    public GameObject realObject;
+    private int numberOfBlockingCollisions;
+    private bool mouseOverSidebar;
+    private GameObject sidebar;
+
+	// Use this for initialization
+	void Start () {
+        //this.gameObject.SetActive(false);
+        numberOfBlockingCollisions = 0;
+        sidebar = GameObject.Find("Sidebar Panel");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //[Ghost Positioning] Centres the ghost on the 1x1 space closest to the cursor
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.x = Mathf.RoundToInt(mousePosition.x);
+        mousePosition.y = Mathf.RoundToInt(mousePosition.y);
+        mousePosition.z = 0;
+
+        this.gameObject.transform.position = mousePosition;
+        //[/Ghost Positioning]
+
+        //[UI Disables Ghost] Stops the ghost logic from running while the mouse is over ui.
+        //!!!WARNING: Collider is a tempory fix and will not work with different screen sizes, use event system instead
+        if(sidebar.GetComponent<Collider2D>().bounds.Contains(Input.mousePosition))
+        {
+            mouseOverSidebar = true;
+        }
+        else
+        {
+            mouseOverSidebar = false;
+        }
+        //[/UI Disables Ghost]
+
+
+        //[Ghost Coloring] colours to the ghost green if they can be safetly placed at their current position
+        //                 and red if something is blocking it
+        if(numberOfBlockingCollisions == 0 && !mouseOverSidebar)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        //[/Ghost Coloring]
+
+        //[Place tile] on a leftclick, places the real version of the selected tile where the ghost was, if the location is unobstructed
+        if (Input.GetMouseButtonDown(0) && numberOfBlockingCollisions == 0 && !mouseOverSidebar)
+        {
+            placeRealObject();
+        }
+        //[/Place tile]
+	}
+
+    void placeRealObject()
+    {
+        Instantiate(realObject, this.gameObject.transform.position, this.gameObject.transform.rotation);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "SolidTile" || col.tag == "WindowTile" || col.tag == "NPC" || col.tag == "Enemy" || col.tag == "Drill")
+        {
+            numberOfBlockingCollisions++;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "SolidTile" || col.tag == "WindowTile" || col.tag == "NPC" || col.tag == "Enemy" || col.tag == "Drill")
+        {
+            numberOfBlockingCollisions--;
+        } 
+    }
+}
---
+++ Bunker Bugs/Assets/PlacingTiles.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacingTiles : MonoBehaviour {
+
+    private int tileIndex;
+    private GameObject steelTileGhost;//reference to the current instance of steel tile ghost
+    private GameObject gruntTileGhost;//reference to the current instance of grunt tile ghost
+
+
+    // Use this for initialization
+    void Start () {
+        tileIndex = 0;//0 means no tile selected
+        steelTileGhost = GameObject.Find("Ghost_Wall");
+        steelTileGhost.SetActive(false);
+        gruntTileGhost = GameObject.Find("Ghost_Grunt");
+        gruntTileGhost.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setSelectedTile_SteelTile()
+    {
+        switch (tileIndex)//deselects the currently selected tile
+        {
+            case 0: steelTileGhost.SetActive(true); tileIndex = 1; break;
+            case 1: steelTileGhost.SetActive(false); tileIndex = 0; break;
+            case 2: steelTileGhost.SetActive(true); gruntTileGhost.SetActive(false); tileIndex = 1; break;
+            default: print("Error: Unknown tile being deselected, check PlacingTiles methods, are the switch statements filled?"); break;
+        }
+    }
+
+    public void setSelectedTile_GruntTile()
+    {
+        switch (tileIndex)//deselects the currently selected tile
+        {
+            case 0: gruntTileGhost.SetActive(true); tileIndex = 2; break;
+            case 1: gruntTileGhost.SetActive(true); steelTileGhost.SetActive(false); tileIndex = 2; break;
+             case 2: gruntTileGhost.SetActive(false); tileIndex = 0;  break;
+             default: print("Error: Unknown tile being deselected, check PlacingTiles methods, are the switch statements filled?"); break;
+        }
+    }
+
+    public void DeSelectAllTiles()
+    {
+        switch (tileIndex)//deselects the currently selected tile
+        {
+            case 0: break;
+            case 1: steelTileGhost.SetActive(false); tileIndex = 0; break;
+            case 2: gruntTileGhost.SetActive(false); tileIndex = 0; break;
+            default: print("Error: Unknown tile being deselected, check PlacingTiles methods, are the switch statements filled?"); break;
+        }
+    }
+
+
+
+
+
+
+}
